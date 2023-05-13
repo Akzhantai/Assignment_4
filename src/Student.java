385af53c154b0cb4072bdbd4870129bfa31eee75
Akzhantai <musabekovaanara17@gmail.com>
2023-05-13 19:29:28 +0600

    Student

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    // Private instance variables
+    private String name;
+    private String gender;
+
+    // Constructor
+    public Student(String name, String gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+
+}
